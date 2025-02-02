27ce8e26ab20cf49b7cf331e6424fa84d7058355
efratGoldshtoff <eg0548449793@gmail.com>
2025-02-02 19:59:39 +0200

    my commit message

---
+++ ToDoListReact/src/service.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+// const apiUrl = "https://localhost:5142"//7083
+axios.defaults.baseURL = 'http://localhost:5142/';//https://localhost:3000/';//7083//5142
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    console.error("Api Request Error: ", error.response ? error.response.data : error.message);
+    return Promise.reject(error);
+  }
+)
+
+export default {
+  //get all tasks
+  getTasks: async () => {
+    const result = await axios.get(`/items`)
+    return result.data;
+  },
+
+  //create a new task
+  addTask: async (name) => {
+    const result = await axios.post(`/items`, { name: name, isComplete: false });
+    return result.data;
+  },
+  //update doing task
+  setCompleted: async (id, name, isComplete) => {
+    if (!id || name === undefined || isComplete === undefined) {
+      console.error("Missing: ", { id, name, isComplete });
+      return;
+    }
+    const taskUpdate = {
+      name: name,
+      isComplete: isComplete
+    }
+    try {
+      const result = await axios.put(`/items/${id}`, taskUpdate);
+      return result.data;
+    } catch (error) {
+      console.error("Error Updating Task")
+      throw error;
+    }
+  },
+  //delete task
+  deleteTask: async (id) => {
+    const result = await axios.delete(`/items/${id}`);
+    return result.data;
+  }
+};
---
+++ TodoApi/Program.cs
@@ -0,0 +1,80 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+builder.Services.AddDbContext<ToDoDbContext>(
+    options =>
+    {
+        options.UseMySql(builder.Configuration.GetConnectionString("todo"),
+        // ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("todo")));
+        new MySqlServerVersion(new Version(8, 0, 0)));
+    });
+
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseCors();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+
+    app.UseSwaggerUI();
+}
+
+//get all tasks
+app.MapGet("/items", async (ToDoDbContext context) =>
+{
+    var tasks = await context.Items.ToListAsync();
+    return Results.Ok(tasks);
+});
+
+//add new task
+app.MapPost("/items", async (ToDoDbContext context, Item newItem) =>
+{
+    await context.Items.AddAsync(newItem);
+    await context.SaveChangesAsync();
+    return Results.Ok(newItem);
+});
+
+//update task
+app.MapPut("/items/{id}", async
+(ToDoDbContext context, int id, Item updatedItem) =>
+{
+    var existItem = await context.Items.FindAsync(id);
+    if (existItem == null)
+        return Results.NotFound($"Item Id: {id} Not Found");
+    existItem.Name = updatedItem.Name;
+    existItem.IsComplete = updatedItem.IsComplete;
+    await context.SaveChangesAsync();
+    return Results.Ok(existItem);
+});
+
+//delete task
+app.MapDelete("/items/{id}", async
+(ToDoDbContext context, int id) =>
+{
+    var item = await context.Items.FindAsync(id);
+    if (item == null)
+        return Results.NotFound($"Item Id: {id} Not Found");
+    context.Items.Remove(item);
+    await context.SaveChangesAsync();
+    return Results.Ok(item);
+});
+
+app.Run();
+
