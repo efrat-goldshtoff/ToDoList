ea17599cce8f15e90ceb546315d813b7b60aca91
efratGoldshtoff <eg0548449793@gmail.com>
2025-02-09 14:22:27 +0200

    i-I

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8b3f9eae127286bc3273019b66bc44ede8c354")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ff53275949cb51f6fb73397ba61b0156b9dde57")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
