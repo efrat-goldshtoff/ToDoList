a1bd60579b3d7fb2f43cbdf656e5c62562b4b42a
efratGoldshtoff <eg0548449793@gmail.com>
2025-02-09 14:33:48 +0200

    add

--- ToDoListReact/src/App.js
+++ ToDoListReact/src/App.js
@@ -6,19 +6,19 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   async function getTodos() {
-      const todos = await service.getTasks();
-      setTodos(todos);
+    const todos = await service.getTasks();
+    setTodos(todos);
   }
 
   async function createTodo(e) {
     e.preventDefault();
-      await service.addTask(newTodo);
-      setNewTodo("");
-      await getTodos();
+    await service.addTask(newTodo);
+    setNewTodo("");
+    await getTodos();
   }
 
   async function updateCompleted(todo, isComplete) {
-    await service.setCompleted(todo.id,todo.name, isComplete);
+    await service.setCompleted(todo.id, todo.name, isComplete);
     await getTodos();
   }
 
@@ -42,7 +42,7 @@ function App() {
       <section className="main" style={{ display: "block" }}>
         <ul className="todo-list">
           {todos.map(todo => {
-            // return (
+            return (
               <li className={todo.isComplete ? "completed" : ""} key={todo.id}>
                 <div className="view">
                   <input className="toggle" type="checkbox" defaultChecked={todo.isComplete} onChange={(e) => updateCompleted(todo, e.target.checked)} />
@@ -50,7 +50,7 @@ function App() {
                   <button className="destroy" onClick={() => deleteTodo(todo.id)}></button>
                 </div>
               </li>
-            // );
+            );
           })}
         </ul>
       </section>
